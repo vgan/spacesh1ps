20f481c4c985955a562a0de161f45b8fca48be87
Steve <vgan@users.noreply.github.com>
2017-08-11 11:18:23 -0400

    Create keys.py
    Added blank keys file.

---
+++ keys.py
@@ -0,0 +1,13 @@
+tumblr_consumer_key = ''
+tumblr_consumer_secret = '' 
+tumblr_token_key = ''
+tumblr_token_secret = ''
+
+twitter_consumer_key = ''
+twitter_consumer_secret = '' 
+twitter_token_key = ''
+twitter_token_secret = ''
+
+mastodon_client_id = ''
+mastodon_client_secret = ''
+mastodon_access_token = ''
